a7a938df5a20916ed1314c2d96e7eef7687838b2
BernardoColorado <bernardocoloradodubois.hw@gmail.com>
2020-12-25 12:09:23 -0600

    agregando controlador de iris para bigquery y owm

--- app/router.php
+++ app/router.php
@@ -1,3 +1,4 @@
 <?php
 
 $app->get('/', App\Infrastructure\Http\Controllers\HomeController::class.':index');
+$app->get('/bigquery-sample/iris', App\Infrastructure\Http\Controllers\IrisController::class.':index');
